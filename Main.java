9c28f0860a68d4b7252e470a9f9fc2c88c71aa14
beazy96 <96582724+beazy96@users.noreply.github.com>
2022-03-22 12:44:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Double z = scanner.nextDouble();
+    Double x = 32 + 9/5 * z;
+    System.out.println(x);
+  }
+}
